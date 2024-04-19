51b5883b26caa15af24634c0b4fea4b616da9e71
e1399579 <e1399579@gmail.com>
2024-04-19 12:51:47 +0800

    静态资源路径修复

--- html/src/js/emoji.js
+++ html/src/js/emoji.js
@@ -1,3 +1,4 @@
+let base_url = process.env.BASE_URL;
 export default [
     {
         label: "长草颜",
@@ -5,122 +6,122 @@ export default [
             {
                 "name": "ywz_1",
                 "title": "不想活了",
-                "src": "/static/emoji/ywz/1.gif"
+                "src": base_url + "/static/emoji/ywz/1.gif"
             },
             {
                 "name": "ywz_2",
                 "title": "不要啦",
-                "src": "/static/emoji/ywz/2.gif"
+                "src": base_url + "/static/emoji/ywz/2.gif"
             },
             {
                 "name": "ywz_3",
                 "title": "买买买",
-                "src": "/static/emoji/ywz/3.gif"
+                "src": base_url + "/static/emoji/ywz/3.gif"
             },
             {
                 "name": "ywz_4",
                 "title": "伤心",
-                "src": "/static/emoji/ywz/4.gif"
+                "src": base_url + "/static/emoji/ywz/4.gif"
             },
             {
                 "name": "ywz_5",
                 "title": "剁剁剁",
-                "src": "/static/emoji/ywz/5.gif"
+                "src": base_url + "/static/emoji/ywz/5.gif"
             },
             {
                 "name": "ywz_6",
                 "title": "加油",
-                "src": "/static/emoji/ywz/6.gif"
+                "src": base_url + "/static/emoji/ywz/6.gif"
             },
             {
                 "name": "ywz_7",
                 "title": "厉害哦",
-                "src": "/static/emoji/ywz/7.gif"
+                "src": base_url + "/static/emoji/ywz/7.gif"
             },
             {
                 "name": "ywz_8",
                 "title": "吃吃吃",
-                "src": "/static/emoji/ywz/8.gif"
+                "src": base_url + "/static/emoji/ywz/8.gif"
             },
             {
                 "name": "ywz_9",
                 "title": "吓尿了",
-                "src": "/static/emoji/ywz/9.gif"
+                "src": base_url + "/static/emoji/ywz/9.gif"
             },
             {
                 "name": "ywz_10",
                 "title": "哈哈",
-                "src": "/static/emoji/ywz/10.gif"
+                "src": base_url + "/static/emoji/ywz/10.gif"
             },
             {
                 "name": "ywz_11",
                 "title": "喜欢你",
-                "src": "/static/emoji/ywz/11.gif"
+                "src": base_url + "/static/emoji/ywz/11.gif"
             },
             {
                 "name": "ywz_12",
                 "title": "对不起",
-                "src": "/static/emoji/ywz/12.gif"
+                "src": base_url + "/static/emoji/ywz/12.gif"
             },
             {
                 "name": "ywz_13",
                 "title": "小鲜肉",
-                "src": "/static/emoji/ywz/13.gif"
+                "src": base_url + "/static/emoji/ywz/13.gif"
             },
             {
                 "name": "ywz_14",
                 "title": "快点",
-                "src": "/static/emoji/ywz/14.gif"
+                "src": base_url + "/static/emoji/ywz/14.gif"
             },
             {
                 "name": "ywz_15",
                 "title": "惊吓",
-                "src": "/static/emoji/ywz/15.gif"
+                "src": base_url + "/static/emoji/ywz/15.gif"
             },
             {
                 "name": "ywz_16",
                 "title": "生日快乐",
-                "src": "/static/emoji/ywz/16.gif"
+                "src": base_url + "/static/emoji/ywz/16.gif"
             },
             {
                 "name": "ywz_17",
                 "title": "有钱",
-                "src": "/static/emoji/ywz/17.gif"
+                "src": base_url + "/static/emoji/ywz/17.gif"
             },
             {
                 "name": "ywz_18",
                 "title": "生气",
-                "src": "/static/emoji/ywz/18.gif"
+                "src": base_url + "/static/emoji/ywz/18.gif"
             },
             {
                 "name": "ywz_19",
                 "title": "美妙极了",
-                "src": "/static/emoji/ywz/19.gif"
+                "src": base_url + "/static/emoji/ywz/19.gif"
             },
             {
                 "name": "ywz_20",
                 "title": "肚子饿",
-                "src": "/static/emoji/ywz/20.gif"
+                "src": base_url + "/static/emoji/ywz/20.gif"
             },
             {
                 "name": "ywz_21",
                 "title": "臭美",
-                "src": "/static/emoji/ywz/21.gif"
+                "src": base_url + "/static/emoji/ywz/21.gif"
             },
             {
                 "name": "ywz_22",
                 "title": "鄙视",
-                "src": "/static/emoji/ywz/22.gif"
+                "src": base_url + "/static/emoji/ywz/22.gif"
             },
             {
                 "name": "ywz_23",
                 "title": "闪瞎啦",
-                "src": "/static/emoji/ywz/23.gif"
+                "src": base_url + "/static/emoji/ywz/23.gif"
             },
             {
                 "name": "ywz_24",
                 "title": "鬼脸",
-                "src": "/static/emoji/ywz/24.gif"
+                "src": base_url + "/static/emoji/ywz/24.gif"
             }
         ],
     },
@@ -130,122 +131,122 @@ export default [
             {
                 "name": "ymj_0",
                 "title": "恭喜发财",
-                "src": "/static/emoji/ymj/0.gif"
+                "src": base_url + "/static/emoji/ymj/0.gif"
             },
             {
                 "name": "ymj_1",
                 "title": "哀求",
-                "src": "/static/emoji/ymj/1.gif"
+                "src": base_url + "/static/emoji/ymj/1.gif"
             },
             {
                 "name": "ymj_2",
                 "title": "加油",
-                "src": "/static/emoji/ymj/2.gif"
+                "src": base_url + "/static/emoji/ymj/2.gif"
             },
             {
                 "name": "ymj_3",
                 "title": "滚",
-                "src": "/static/emoji/ymj/3.gif"
+                "src": base_url + "/static/emoji/ymj/3.gif"
             },
             {
                 "name": "ymj_4",
                 "title": "哇",
-                "src": "/static/emoji/ymj/4.gif"
+                "src": base_url + "/static/emoji/ymj/4.gif"
             },
             {
                 "name": "ymj_5",
                 "title": "好委屈",
-                "src": "/static/emoji/ymj/5.gif"
+                "src": base_url + "/static/emoji/ymj/5.gif"
             },
             {
                 "name": "ymj_6",
                 "title": "我滚啦",
-                "src": "/static/emoji/ymj/6.gif"
+                "src": base_url + "/static/emoji/ymj/6.gif"
             },
             {
                 "name": "ymj_7",
                 "title": "你慢慢吃",
-                "src": "/static/emoji/ymj/7.gif"
+                "src": base_url + "/static/emoji/ymj/7.gif"
             },
             {
                 "name": "ymj_8",
                 "title": "好烦",
-                "src": "/static/emoji/ymj/8.gif"
+                "src": base_url + "/static/emoji/ymj/8.gif"
             },
             {
                 "name": "ymj_9",
                 "title": "干不掉我",
-                "src": "/static/emoji/ymj/9.gif"
+                "src": base_url + "/static/emoji/ymj/9.gif"
             },
             {
                 "name": "ymj_10",
                 "title": "出去玩",
-                "src": "/static/emoji/ymj/10.gif"
+                "src": base_url + "/static/emoji/ymj/10.gif"
             },
             {
                 "name": "ymj_11",
                 "title": "哈哈",
-                "src": "/static/emoji/ymj/11.gif"
+                "src": base_url + "/static/emoji/ymj/11.gif"
             },
             {
                 "name": "ymj_12",
                 "title": "睡觉",
-                "src": "/static/emoji/ymj/12.gif"
+                "src": base_url + "/static/emoji/ymj/12.gif"
             },
             {
                 "name": "ymj_13",
                 "title": "看不惯我",
-                "src": "/static/emoji/ymj/13.gif"
+                "src": base_url + "/static/emoji/ymj/13.gif"
             },
             {
                 "name": "ymj_14",
                 "title": "咋啦",
-                "src": "/static/emoji/ymj/14.gif"
+                "src": base_url + "/static/emoji/ymj/14.gif"
             },
             {
                 "name": "ymj_15",
                 "title": "你最好了",
-                "src": "/static/emoji/ymj/15.gif"
+                "src": base_url + "/static/emoji/ymj/15.gif"
             },
             {
                 "name": "ymj_16",
                 "title": "保佑我",
-                "src": "/static/emoji/ymj/16.gif"
+                "src": base_url + "/static/emoji/ymj/16.gif"
             },
             {
                 "name": "ymj_17",
                 "title": "不",
-                "src": "/static/emoji/ymj/17.gif"
+                "src": base_url + "/static/emoji/ymj/17.gif"
             },
             {
                 "name": "ymj_18",
                 "title": "嗯",
-                "src": "/static/emoji/ymj/18.gif"
+                "src": base_url + "/static/emoji/ymj/18.gif"
             },
             {
                 "name": "ymj_19",
                 "title": "嗨",
-                "src": "/static/emoji/ymj/19.gif"
+                "src": base_url + "/static/emoji/ymj/19.gif"
             },
             {
                 "name": "ymj_20",
                 "title": "么么哒",
-                "src": "/static/emoji/ymj/20.gif"
+                "src": base_url + "/static/emoji/ymj/20.gif"
             },
             {
                 "name": "ymj_21",
                 "title": "刚怎么了",
-                "src": "/static/emoji/ymj/21.gif"
+                "src": base_url + "/static/emoji/ymj/21.gif"
             },
             {
                 "name": "ymj_22",
                 "title": "好棒",
-                "src": "/static/emoji/ymj/22.gif"
+                "src": base_url + "/static/emoji/ymj/22.gif"
             },
             {
                 "name": "ymj_23",
                 "title": "别闹",
-                "src": "/static/emoji/ymj/23.gif"
+                "src": base_url + "/static/emoji/ymj/23.gif"
             }
         ],
     },
